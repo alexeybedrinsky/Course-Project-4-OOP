dfe1a225d309231caf7827bbc85f06daec264edf
Alexey Bedrinsky <bedrinsky@mail.ru>
2024-07-18 17:11:58 +0300

    Update vacancies_code.py
    улучшен код согласно рекомендациям преподавателя, добавлены комментарии через докстринги

--- data/vacancies_code.py
+++ data/vacancies_code.py
@@ -1,6 +1,70 @@
 import requests
 import json
 import os
+from abc import ABC, abstractmethod
+
+# Абстрактный класс для работы с файлами
+class FileHandler(ABC):
+    """
+    Абстрактный класс для обработки файлов.
+    """
+    @abstractmethod
+    def read(self):
+        """
+        Чтение данных из файла.
+        """
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        """
+        Запись данных в файл.
+        """
+        pass
+
+    @abstractmethod
+    def append(self, data):
+        """
+        Добавление данных в файл.
+        """
+        pass
+
+
+class JSONFileHandler(FileHandler):
+    """
+    Класс для обработки JSON файлов.
+    """
+    def __init__(self, filename):
+        self._filename = filename
+
+    def read(self):
+        """
+        Чтение данных из JSON файла.
+        :return: Данные из файла или пустой список, если файл не существует или содержит некорректные данные.
+        """
+        if os.path.exists(self._filename):
+            try:
+                with open(self._filename, 'r', encoding='utf-8') as f:
+                    return json.load(f)
+            except json.JSONDecodeError:
+                # Если файл пуст или содержит некорректные данные -> возвращаем пустой список
+                return []
+        return []
+
+    def write(self, data):
+        """
+        Запись данных в файл.
+        """
+        with open(self._filename, 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+    def append(self, data):
+        """
+        Добавление данных в файл.
+        """
+        current_data = self.read()
+        current_data.extend(data)
+        self.write(current_data)
 
 # URL API для поиска вакансий на hh.ru
 url = 'https://api.hh.ru/vacancies'
@@ -24,11 +88,12 @@ if response.status_code == 200:
     current_dir = os.path.dirname(__file__)
     json_path = os.path.join(current_dir, 'vacancies.json')
 
-    # Сохранение данных в файл JSON
-    with open(json_path, 'w', encoding='utf-8') as f:
-        json.dump(vacancies, f, ensure_ascii=False, indent=4)
+    # Создание объекта JSONFileHandler
+    json_handler = JSONFileHandler(json_path)
 
-    print(f"Данные успешно записаны в файл {json_path}")
+    # Добавление данных в файл JSON
+    json_handler.append(vacancies)
+
+    print(f"Данные успешно добавлены в файл {json_path}")
 else:
     print(f"Ошибка при выполнении запроса: {response.status_code}")
-
