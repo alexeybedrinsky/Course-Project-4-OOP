1c35771a56851b92156a659fc1e4775d11295e62
Alexey Bedrinsky <bedrinsky@mail.ru>
2024-07-18 17:16:38 +0300

    Update JSONVacancyStorage.py
    улучшен код и добавлены описания

--- src/JSONVacancyStorage.py
+++ src/JSONVacancyStorage.py
@@ -1,77 +1,124 @@
 import json
+from abc import ABC, abstractmethod
 from src.Vacancy import Vacancy
 
-class JSONVacancyStorage:
+# Абстрактный класс для работы с файлами
+class FileHandler(ABC):
+    """
+    Абстрактный класс для обработки файлов.
+    """
+    @abstractmethod
+    def read(self):
+        """
+        Чтение файла
+        """
+        pass
+
+    @abstractmethod
+    def write(self, data):
+        """
+        Запись в файл
+        """
+        pass
+
+    @abstractmethod
+    def append(self, data):
+        """
+        Добавление в файл
+        """
+        pass
+
+# Класс для работы с JSON файлами, наследуемый от абстрактного класса
+class JSONVacancyStorage(FileHandler):
+    """
+    Класс для работы с вакансиями в формате JSON.
+    """
     def __init__(self, file_path='data/vacancies.json'):
-        self.file_path = file_path
+        self._file_path = file_path  # Приватный атрибут
 
-    def get_vacancies(self):
+    def read(self):
+        """
+        Чтение из Джейсона
+        """
         try:
-            with open(self.file_path, 'r', encoding='utf-8') as file:
-                data = json.load(file)
-
-            vacancies = []
-            for vacancy in data:
-                if vacancy is None:
-                    print("Warning: Found NoneType vacancy. Skipping.")
-                    continue
-
-                title = vacancy.get('title', '')
-                company = vacancy.get('company', '')
-                salary = vacancy.get('salary', 'Не указана')
-                description = vacancy.get('description', '')
-                link = vacancy.get('link', '')
-
-                vacancies.append(Vacancy(
-                    title=title,
-                    company=company,
-                    salary=salary,
-                    description=description,
-                    link=link
-                ))
-            return vacancies
+            with open(self._file_path, 'r', encoding='utf-8') as file:
+                return json.load(file)
         except FileNotFoundError:
-            print(f"Error: File {self.file_path} not found.")
+            print(f"Error: File {self._file_path} not found.")
             return []
         except json.JSONDecodeError:
             print("Error: JSON decode error. Please check the JSON file format.")
             return []
 
-    def save_vacancies(self, vacancies):
-        with open(self.file_path, 'w', encoding='utf-8') as file:
-            json.dump([vacancy.__dict__ for vacancy in vacancies], file, ensure_ascii=False, indent=4)
-
-    def delete_vacancy(self, vacancy):
-        try:
-            with open(self.file_path, "r", encoding="utf-8") as file:
-                data = json.load(file)
-        except FileNotFoundError:
-            data = []
-
-        data = [v for v in data if v['title'] != vacancy.title or v['company'] != vacancy.company]
-
-        with open(self.file_path, "w", encoding="utf-8") as file:
+    def write(self, data):
+        """
+        Запись в Джейсон
+        """
+        with open(self._file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
 
-    def find_vacancies_with_keyword(self, keyword):
-        try:
-            with open(self.file_path, 'r', encoding='utf-8') as file:
-                data = json.load(file)
+    def append(self, data):
+        """
+        Добавление в Лжейсон
+        """
+        current_data = self.read()
+        current_data.extend(data)
+        self.write(current_data)
 
-            matching_vacancies = []
-            for vacancy in data:
-                if keyword.lower() in vacancy.get('description', '').lower():
-                    matching_vacancies.append(Vacancy(
-                        title=vacancy.get('title', ''),
-                        company=vacancy.get('company', ''),
-                        salary=vacancy.get('salary', 'Не указана'),
-                        description=vacancy.get('description', ''),
-                        link=vacancy.get('link', '')
-                    ))
-            return matching_vacancies
-        except FileNotFoundError:
-            print(f"Error: File {self.file_path} not found.")
-            return []
-        except json.JSONDecodeError:
-            print("Error: JSON decode error. Please check the JSON file format.")
-            return []
+    def get_vacancies(self):
+        """
+        Получение списка вакансий из Джейсона
+        """
+        data = self.read()
+        vacancies = []
+        for vacancy in data:
+            if vacancy is None:
+                print("Warning: Found NoneType vacancy. Skipping.")
+                continue
+
+            title = vacancy.get('title', '')
+            company = vacancy.get('company', '')
+            salary = vacancy.get('salary', 'Не указана')
+            description = vacancy.get('description', '')
+            link = vacancy.get('link', '')
+
+            vacancies.append(Vacancy(
+                title=title,
+                company=company,
+                salary=salary,
+                description=description,
+                link=link
+            ))
+        return vacancies
+
+    def save_vacancies(self, vacancies):
+        """
+        Сохранение списка вакансий в Джейсон
+        """
+        self.write([vacancy.__dict__ for vacancy in vacancies])
+
+    def delete_vacancy(self, vacancy):
+        """
+        Удаление вакансий из Джейсона
+        """
+        data = self.read()
+        data = [v for v in data if v['title'] != vacancy.title or v['company'] != vacancy.company]
+        self.write(data)
+
+    def find_vacancies_with_keyword(self, keyword):
+        """
+        Поиск вакансий по ключслову в описании
+        """
+        data = self.read()
+        matching_vacancies = []
+        for vacancy in data:
+            description = vacancy.get('description', '')
+            if description and keyword.lower() in description.lower():
+                matching_vacancies.append(Vacancy(
+                    title=vacancy.get('title', ''),
+                    company=vacancy.get('company', ''),
+                    salary=vacancy.get('salary', 'Не указана'),
+                    description=vacancy.get('description', ''),
+                    link=vacancy.get('link', '')
+                ))
+        return matching_vacancies
