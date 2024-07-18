f458795c37c92bfbc2f189d1f172fc5f5ec042ed
Alexey Bedrinsky <bedrinsky@mail.ru>
2024-07-18 17:15:20 +0300

    Update Abstract_class_and_API.py
    улучшен код и добавлено описание действий

--- src/Abstract_class_and_API.py
+++ src/Abstract_class_and_API.py
@@ -3,13 +3,31 @@ import requests
 
 
 class AbstractVacancyAPI(ABC):
+    """
+    Абстрактный класс для API вакансий.
+    """
     @abstractmethod
     def get_vacancies(self, search_query, per_page=100):
+        """
+        Получение списка вакансий по запросу.
+        :param search_query: Строка поиска.
+        :param per_page: Количество вакансий на одной странице (по умолчанию 100).
+        :return: Список вакансий.
+        """
         pass
 
 
 class HeadHunterAPI(AbstractVacancyAPI):
+    """
+    Класс для работы с API HeadHunter.
+    """
     def get_vacancies(self, query, per_page=100):
+        """
+        Получение списка вакансий c HH по запросу.
+        :param search_query: Строка поиска.
+        :param per_page: Количество вакансий на одной странице (по умолчанию 100).
+        :return: Список вакансий.
+        """
         url = f'https://api.hh.ru/vacancies?text={query}&per_page={per_page}'
         response = requests.get(url)
 
@@ -26,3 +44,4 @@ if __name__ == "__main__":
     vacancies = api.get_vacancies("Python developer")
     for vacancy in vacancies:
         print(vacancy)
+
